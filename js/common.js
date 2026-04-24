80d372ecf7ba076c85cb1cc1e8b5fa7bb3cb079f
SagarCode_Labs <sagarnachankar77@gmail.com>
2026-04-24 15:27:14 +0530

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,5 +1,5 @@
 ﻿function loadNavbar() {
-    fetch('components/navbar.html')
+    fetch('/components/navbar.html')
         .then(res => res.text())
         .then(data => {
             document.getElementById("navbar").innerHTML = data;
